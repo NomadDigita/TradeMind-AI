23a90c84c58ee50f23aae4e92812ea2ebd1eb34d
FavourOnome <fonome519@gmail.com>
2026-06-04 02:07:11 +0100

    fix: append .js module file extension in telegramService relative imports

--- backend/src/services/telegramService.js
+++ backend/src/services/telegramService.js
@@ -1,5 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
-import { CONFIG } from '../config/constants';
+import { CONFIG } from '../config/constants.js';
 import { BitgetService } from './bitgetService.js';
 import { AnalyzerService } from './analyzerService.js';
 
